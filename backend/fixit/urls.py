eded575b9dc396a3594408c1110a097d98655917
Meitouhv45 <marcialrogeliojr27@gmail.com>
2025-09-16 00:36:49 +0800

    Initial commit

--- backend/fixit/urls.py
+++ backend/fixit/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for fixit_service project.
+URL configuration for fixit project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,18 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.urls import path
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),  # Root URL
     path('admin/', admin.site.urls),
-    path('ticket/', include('reports.urls')),
-    path('chat/', include('chat.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
